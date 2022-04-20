6f7405436e5754c038df603c49845621e91afebb
Gabriel Poersch <gabpoersch@gmail.com>
2022-04-20 17:21:34 -0300

    Spring Initializr

---
+++ colabore/src/main/java/br/com/dbc/devser/colabore/ColaboreApplication.java
@@ -0,0 +1,13 @@
+package br.com.dbc.devser.colabore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColaboreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColaboreApplication.class, args);
+	}
+
+}
---
+++ colabore/src/test/java/br/com/dbc/devser/colabore/ColaboreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dbc.devser.colabore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColaboreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
