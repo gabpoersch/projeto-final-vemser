4cb1b3c3e76c4059af9e256f43f2ed2518ffb2c4
Ezequias Ferreira <ezequiasbarros1997@gmail.com>
2022-04-20 21:55:31 -0300

    feat: criada a entidade de fundraiser

---
+++ colabore/src/main/java/br/com/dbc/devser/colabore/controller/FundraiserController.java
@@ -0,0 +1,4 @@
+package br.com.dbc.devser.colabore.controller;
+
+public class FundraiserController {
+}
--- colabore/src/main/java/br/com/dbc/devser/colabore/entity/FundraiserEntity.java
+++ colabore/src/main/java/br/com/dbc/devser/colabore/entity/FundraiserEntity.java
@@ -1,18 +1,47 @@
 package br.com.dbc.devser.colabore.entity;
 
-import java.time.LocalDateTime;
+import lombok.*;
 
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "fundraiser")
 public class FundraiserEntity {
-    private Integer fundraiserId;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "GEN_FUNDRAISER")
+    @SequenceGenerator(name = "GEN_FUNDRAISER", sequenceName = "sequence_fundraiser", allocationSize = 1)
+    @Column(name = "fundraiser_id")
+    private Long fundraiserId;
+
     private String title;
+
     private String description;
-    private String goal;
-    private String currentValue;
+
+    private Double goal;
+
+    @Column(name = "current_value")
+    private Double currentValue;
+
+    @Column(name = "cover_photo")
+    private String coverPhoto;
+
     private Boolean status;
+
     private Category category;
-    private LocalDateTime creationDate;
-    //PEGAR DE USER ENTITY
-    private Integer userId;
 
+    @Column(name = "creation_date")
+    private Long creationDate;
 
+    @Column(name = "automatic_close")
+    private Boolean automaticClose;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "fundraiser_creator", referencedColumnName = "user_id")
+    @Column(name = "fundraiser_creator")
+    private UserEntity fundraiseCreator;
 }
