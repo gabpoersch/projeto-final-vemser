66abf5f0a0887b3ab287526c151828bddba8b9ba
Ezequias Ferreira <ezequiasbarros1997@gmail.com>
2022-04-28 19:50:23 -0300

    Fix homecolabore

--- src/test/java/br/com/dbc/devser/colabore/functional/pageObjects/HomeColabore.java
+++ src/test/java/br/com/dbc/devser/colabore/functional/pageObjects/HomeColabore.java
@@ -2,7 +2,7 @@ package br.com.dbc.devser.colabore.functional.pageObjects;
 
 import org.openqa.selenium.By;
 
-public class HeaderColabore {
+public class HomeColabore {
     public static By exploreButton = By.xpath("//*[@id=\"root\"]/header/nav/ul/li[1]/a");
     public static By searchBar = By.xpath("//*[@id=\"root\"]/header/nav/ul/li[2]/div");
     public static By createButton = By.xpath("//*[@id=\"root\"]/header/nav/ul/li[3]/button");
